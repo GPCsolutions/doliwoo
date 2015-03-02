a6018fe7d53cec2d710eb5eea00d15aa43f55f5e
Raphaël Doursenaud <rdoursenaud@gpcsolutions.fr>
2015-03-02 15:20:53 +0100

    BROKEN: Italian taxes try

--- includes/class-wc-tax.php
+++ includes/class-wc-tax.php
@@ -108,6 +108,30 @@ class Doliwoo_WC_Tax extends WC_Tax {
 				'tax_rate_order'    => '0',
 				'tax_rate_class'    => 'zero',
 			),
+			array(
+				'tax_rate_country'  => 'IT',
+				'tax_rate'          => '22',
+				'tax_rate_name'     => $tax_name,
+				'tax_rate_priority' => '1',
+				'tax_rate_order'    => '0',
+				'tax_rate_class'    => '',
+			),
+			array(
+				'tax_rate_country'  => 'IT',
+				'tax_rate'          => '10',
+				'tax_rate_name'     => $tax_name,
+				'tax_rate_priority' => '1',
+				'tax_rate_order'    => '0',
+				'tax_rate_class'    => 'reduced',
+			),
+			array(
+				'tax_rate_country'  => 'IT',
+				'tax_rate'          => '4',
+				'tax_rate_name'     => $tax_name,
+				'tax_rate_priority' => '1',
+				'tax_rate_order'    => '0',
+				'tax_rate_class'    => 'super-reduced',
+			),
 		);
 
 		$db_taxes = $wpdb->get_results(
