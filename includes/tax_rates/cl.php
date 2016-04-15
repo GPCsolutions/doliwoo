a44b0ef9c488dce6ac9fe3ec398223f623dfba62
Hector Tolmo <SoftwareforPyme@users.noreply.github.com>
2016-04-14 22:14:20 -0300

    Create cl.php
    Hector Torres Tolmo contribution of SoftwareforPyme.com

---
+++ includes/tax_rates/cl.php
@@ -0,0 +1,19 @@
+<?php
+$declared_rates = array(
+	array(
+		'tax_rate_country'  => 'CL',
+		'tax_rate'          => '19',
+		'tax_rate_name'     => $tax_name,
+		'tax_rate_priority' => '1',
+		'tax_rate_order'    => '0',
+		'tax_rate_class'    => '',
+	),
+		array(
+		'tax_rate_country'  => 'CL',
+		'tax_rate'          => '0',
+		'tax_rate_name'     => $tax_name,
+		'tax_rate_priority' => '2',
+		'tax_rate_order'    => '0',
+		'tax_rate_class'    => 'exento',
+	),
+);
